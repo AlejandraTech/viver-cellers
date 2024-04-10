c108a1dcb4613911df87b1744a6c3fa6a13575af
Alejandra <pazrivasalejandra@gmail.com>
2024-04-10 10:28:12 +0200

    Angular Validations Implementation for Login Form
    Implementación de las validaciones necesarias en el formulario de login en Angular.

--- Angular/viver-cellers/src/app/components/login/login.component.ts
+++ Angular/viver-cellers/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  // Define form control property of type formGroup.
+  loginForm!: FormGroup;
 
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern("^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$")]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    });
+  }
+
+  // Submit the login form.
+  submitLoginForm(): void {
+    if (this.loginForm.valid) {
+      console.log("Validaciones correctas");
+    } else {
+      console.log("El formulario no es válido");
+    }
+  }
 }
