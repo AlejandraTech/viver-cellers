368800a171ce1a7d146a15cf4c6a04dc62ba34f3
Alejandra <pazrivasalejandra@gmail.com>
2024-04-28 13:19:10 +0200

    Dynamic Home Page
    Modificación de la página de home para que se vea de forma dinámica.

--- Laravel/viver-cellers/routes/api.php
+++ Laravel/viver-cellers/routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\PurchaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
