4143efe90060dfc2a4802ba47da68f44ba628d33
Alejandra <pazrivasalejandra@gmail.com>
2024-05-03 14:19:56 +0200

    Modification in style
    Modificación en el estilo.

--- Angular/viver-cellers/src/app/components/info-project/info-project.component.ts
+++ Angular/viver-cellers/src/app/components/info-project/info-project.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Project } from 'src/app/models/Project';
+import { ProductService } from 'src/app/services/product.service';
 import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
@@ -9,15 +10,45 @@ import { ProjectService } from 'src/app/services/project.service';
   styleUrls: ['./info-project.component.css']
 })
 export class InfoProjectComponent implements OnInit {
+  // Array to store projects fetched from the database
   projects: any[] = [];
 
-  constructor(private service: ProjectService, private route: ActivatedRoute) { }
+  // Array to store products fetched from the database
+  products: any[] = [];
 
+  project_id!: number;
+
+  constructor(private service: ProjectService, private serviceProducts: ProductService, private route: ActivatedRoute) { }
+
+  /**
+   * Method executed when the component initializes
+   */
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
 
-    this.service.getProjectDetails(id).subscribe(data => {
+    //get the project id and convert it to number
+    this.project_id = Number(this.route.snapshot.paramMap.get('id'));
+
+    //call the service to get all the information about the project
+    this.service.getProjectDetails(this.project_id).subscribe(data => {
       this.projects = [data];
     });
+
+    // Get the list of product and available vineyard
+    this.getProduct();
+  }
+
+  /**
+   * Method to fetch the list of product from the service
+   */
+  getProduct(): void {
+    this.serviceProducts.getProduct().subscribe(
+      data => {
+        this.products = data.data;
+        console.log(this.products);
+      },
+      error => {
+        console.error(`Error en la recuperació de productes:`, error);
+      }
+    );
   }
 }
