39b14fe193ebbcded6f88f56021600dda3928f57
Alejandra <pazrivasalejandra@gmail.com>
2024-05-14 11:31:28 +0200

    Improved web page views
    Se han mejorado el estilo de las vistas de la página web.

--- Angular/viver-cellers/src/app/app.component.ts
+++ Angular/viver-cellers/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'viver-cellers';
+
+  ngAfterViewInit() {
+    initFlowbite();
+  }
 }
